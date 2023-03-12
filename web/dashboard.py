dc2b565fff3b739796a67ce7a44ae8975204fff8
Yujie Zhu <yzhu@Yujies-MacBook-Pro.local>
2023-03-11 22:00:41 -0800

    data test

--- web/dashboard.py
+++ web/dashboard.py
@@ -9,12 +9,12 @@ class DashboardController():
     def response(self):
         response = ""
         path = os.path.dirname(os.path.abspath(__file__)) + "/dashboard.html"
-        parameters = f"""
-                    var xValues1 = [50,60,70,80,90,100,110,120,130,140,150];/n
-                    var yValues1 = [7,8,8,9,9,9,10,11,14,14,15];/n
-                    var xValues2 = [50,60,70,80,90,100,110,120,130,140,150];/n
-                    var yValues2 = [7,8,8,9,9,9,10,11,14,14,15];/n
-                    """
+        parameters = f"""\
+var xValues1 = [50,60,70,80,90,100,110,120,130,140,150];
+var yValues1 = [7,8,8,9,9,9,10,11,14,14,15];
+var xValues2 = [50,60,70,80,90,100,110,120,130,140,150];
+var yValues2 = [7,8,8,9,9,9,10,11,14,14,15];
+"""
         with open(path) as f:
             for line in f:
                 if "{% customer insert indicator %}" in line:
